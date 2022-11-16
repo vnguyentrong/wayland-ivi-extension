ee9f030bdee5a15383c6e8744ab1d52089b51931
Tran Ba Khang(MS/EMC31-XC) <Khang.TranBa@vn.bosch.com>
2022-11-16 14:43:21 +0700

    weston-ivi-shell: switch to weston_load_module
    weston removed the wet_load_module_entrypoint, need switch to
    weston_load_module.
    
    Signed-off-by: Tran Ba Khang(MS/EMC31-XC) <Khang.TranBa@vn.bosch.com>

--- weston-ivi-shell/src/ivi-controller.c
+++ weston-ivi-shell/src/ivi-controller.c
@@ -1993,7 +1993,7 @@ load_input_module(struct ivishell *shell)
         return 0;
     }
 
-    input_module_init = wet_load_module_entrypoint(input_module, "input_controller_module_init");
+    input_module_init = weston_load_module(input_module, "input_controller_module_init", NULL);
     if (!input_module_init)
         return -1;
 
@@ -2065,7 +2065,7 @@ static int load_id_agent_module(struct ivishell *shell)
         return 0;
     }
 
-    id_agent_module_init = wet_load_module_entrypoint(id_agent_module, "id_agent_module_init");
+    id_agent_module_init = weston_load_module(id_agent_module, "id_agent_module_init", NULL);
     if (!id_agent_module_init)
         return -1;
 
